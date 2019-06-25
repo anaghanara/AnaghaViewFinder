a084b37570cd58606f9db5af5b6aad407c04a41d
Apple <apple@Apples-MacBook-Pro-9.local>
2019-06-25 14:57:45 -0500

    Iteration 2 done

--- AnaghaViewFinder/AddPhotoViewController.swift
+++ AnaghaViewFinder/AddPhotoViewController.swift
@@ -14,6 +14,8 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
     
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var captionText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
@@ -27,6 +29,25 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
     @IBAction func albumTapped(_ sender: UIButton) {
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated : true, completion :nil)
+    }
+    
+    @IBAction func savePhoto(_ sender: UIButton) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            
+            let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            
+            photoToSave.caption = captionText.text
+            
+            if let userImage = imageView.image {
+                
+                if let userImageData = userImage.pngData() {
+                    photoToSave.imageData = userImageData
+                }
+            }
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+                navigationController?.popViewController(animated: true)
+        }
+
     }
     
         
