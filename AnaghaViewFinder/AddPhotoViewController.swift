784c74b949e94c872081d29bf7a033b7886eb11c
Apple <apple@Apples-MacBook-Pro-9.local>
2019-06-25 11:46:58 -0500

    Iteration 1 Done

---
+++ AnaghaViewFinder/AddPhotoViewController.swift
@@ -0,0 +1,55 @@
+//
+//  AddPhotoViewController.swift
+//  AnaghaViewFinder
+//
+//  Created by Apple on 6/25/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController ()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated : true, completion :nil)
+    }
+    
+    @IBAction func albumTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated : true, completion :nil)
+    }
+    
+        
+        // Do any additional setup after loading the view.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = selectedImage
+            
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
+        }
+    
+  
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
